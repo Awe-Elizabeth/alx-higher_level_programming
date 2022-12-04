19e675dc9c3cd46324808677aec499fbbeae4d04
Awe Elizabeth <alizzyshire@gmail.com>
2022-12-04 12:50:11 -0800

    Imo

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import argv from sys
+    from sys import argv
 
     sum = 0
 
     for i in ramge(len(argv) - 1):
-         sum += int(argv[i + 1])
+        sum += int(argv[i + 1])
     print("{}".format(sum))
