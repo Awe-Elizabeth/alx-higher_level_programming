1c7430c4fa3f7aa7156360e3d7516fc86e92a834
Awe Elizabeth <alizzyshire@gmail.com>
2022-12-04 12:03:30 -0800

    Imports

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import argv from sys
+
+    sum = 0
+
+    for i in ramge(len(argv) - 1):
+         sum += int(argv[i + 1])
+    print("{}".format(sum))
