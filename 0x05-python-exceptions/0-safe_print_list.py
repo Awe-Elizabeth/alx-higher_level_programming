7f4c255fd017a6a425b3ad160f4427e5c62e29cf
Awe Elizabeth <alizzyshire@gmail.com>
2022-12-21 06:08:40 -0800

    Commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    number = 0
-    for i in range(x):
+    num = 0
+    for item in range(x):
         try:
-            print(my_list[i], end='')
-            number += 1
+            print(my_list[item], end='')
+            num += 1
         except Exception:
             break
-        print('')
-        return number
+    print('')
+    return num
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    num = 0
+    n = 0
     for i in range(x):
+        item = my_list[i]
         try:
-             print("{:d}".format(my_list[i], end=''))
-             num += 1
+            print("{:d}".format(item), end="")
+            n += 1
         except Exception:
             continue
     print("")
-    return num
+    return n
